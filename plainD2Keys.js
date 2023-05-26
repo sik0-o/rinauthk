abdf2bffdbaee104b4eb96db16d0921c8f9e2c53
Evgeny Netesov <siko.sik.1@gmail.com>
2023-05-26 05:32:57 +0300

    Update plainD2Keys.js

--- plainD2Keys.js
+++ plainD2Keys.js
@@ -1,5 +1,16 @@
 const rinauthk = {
     "D2": "f4a9d89a-0898-45a4-8e51-1427ed6bd27a",
+    "k": [
+        "f4a9d89a-0198-45a4-8e51-1427ed6bd27a",
+        "f4a9d89a-0298-45a4-8e51-1427ed6bd27a",
+        "f4a9d89a-0398-45a4-8e51-1427ed6bd27a",
+        "f4a9d89a-0498-45a4-8e51-1427ed6bd27a",
+        "f4a9d89a-0598-45a4-8e51-1427ed6bd27a",
+        "f4a9d89a-0698-45a4-8e51-1427ed6bd27a",
+        "f4a9d89a-0798-45a4-8e51-1427ed6bd27a",
+        "f4a9d89a-0898-45a4-8e51-1427ed6bd27a",
+        "f4a9d89a-0998-45a4-8e51-1427ed6bd27a"
+    ],
     "init": () => {console.log('kservice initialized');}
 };
 rinauthk.init();
